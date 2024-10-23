c6e6b8ddd62442d8960fbb9a099c253e7eb54df2
Walssimon <walssimon123@gmail.com>
2024-10-23 12:11:22 -0300

    javaScript finalizado

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
-let currentIndex = 0;
+var formulario = document.getElementById('frmContato')
+var  nome = document.getElementById('txtNome')
+
+let currentIndex = 0
+const items = document.querySelectorAll('.carousel-item')
+const totalItems = items.length
+
+function updateCarousel() {
+        
+    const offset = -currentIndex * 800
+    document.querySelector('.carousel').style.transform = `translateX(${offset}px)`
+}
 
 function moveSlide(direction) {
-  const carousel = document.querySelector('.carousel');
-  const totalItems = document.querySelectorAll('.carousel-item').length;
-
-  currentIndex += direction;
-
-  if (currentIndex < 0) {
-    currentIndex = totalItems - 1;
-  } else if (currentIndex >= totalItems) {
-    currentIndex = 0;
-  }
-
-  carousel.style.transform = `translateX(-${currentIndex * 100}%)`;
+    currentIndex += direction
+    if (currentIndex < 0) {
+        currentIndex = totalItems - 1
+    } else if (currentIndex >= totalItems) {
+        currentIndex = 0
+    }
+    updateCarousel()
 }
+
+formulario.addEventListener('submit', function(event) 
+{
+    if(nome.value == ' ')
+    {
+            alert('Digite um nome')
+    }else if(nome.value =! null)
+        {
+                alert('Formulario enviado com sucesso')
+        }
+})
+
